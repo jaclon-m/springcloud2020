59272b45e72d9751409e5a0bde706d8c261792ec
jaclon-m <jaclon_m@163.com>
2021-04-16 10:02:06 +0800

    spring cloud - sentinel

---
+++ cloudalibaba-sentinel-server8401/src/main/java/com/jaclon/springcloud/MainApp8401.java
@@ -0,0 +1,17 @@
+package com.jaclon.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author jaclon
+ * @date 2021/4/16
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MainApp8401 {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApp8401.class,args);
+    }
+}
---
+++ cloudalibaba-sentinel-server8401/src/main/java/com/jaclon/springcloud/controller/RateLimitController.java
@@ -0,0 +1,11 @@
+package com.jaclon.springcloud.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author jaclon
+ * @date 2021/4/16
+ */
+@RestController
+public class RateLimitController {
+}
