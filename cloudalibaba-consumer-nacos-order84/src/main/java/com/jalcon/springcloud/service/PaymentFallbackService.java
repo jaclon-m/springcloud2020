02d7451527c1c719937e9e21198e12754b81d97c
jaclon-m <jaclon_m@163.com>
2021-04-19 15:40:43 +0800

    spring cloud - sentinel

--- cloudalibaba-consumer-nacos-order84/src/main/java/com/jalcon/springcloud/service/PaymentFallbackService.java
+++ cloudalibaba-consumer-nacos-order84/src/main/java/com/jalcon/springcloud/service/PaymentFallbackService.java
@@ -2,6 +2,7 @@ package com.jalcon.springcloud.service;
 
 import com.jaclon.springcloud.entity.CommonResult;
 import com.jaclon.springcloud.entity.Payment;
+import org.springframework.stereotype.Service;
 
 /**
  * TODO Description
@@ -9,6 +10,7 @@ import com.jaclon.springcloud.entity.Payment;
  * @author jaclon
  * @since 2021/4/19 15:30
  */
+@Service
 public class PaymentFallbackService implements PaymentService{
     @Override
     public CommonResult<Payment> paymentSQL(Long id) {
